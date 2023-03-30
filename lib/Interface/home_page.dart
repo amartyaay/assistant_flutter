b0bbb1a36ff49a3c07066054bea3612d2015f986
Amartya <62711821+amartyaay@users.noreply.github.com>
2023-03-30 16:47:52 +0530

    implemented t2s and s2t with chatgpt

--- lib/Interface/home_page.dart
+++ lib/Interface/home_page.dart
@@ -1,108 +1,11 @@
 import 'package:assistant/constants/color.dart';
+import 'package:assistant/core/openai.dart';
 import 'package:assistant/widgets/feature_box.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-class HomePage extends HookConsumerWidget {
-  const HomePage({super.key});
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Assistant'),
-        leading: const Icon(Icons.menu),
-      ),
-      body: Column(
-        children: [
-          //Assistant picture
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
-                  decoration: const BoxDecoration(
-                    color: ColorsTheme.assistantCircleColor,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-              ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/virtualAssistant.png',
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-          //Chat bubble
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
-            decoration: BoxDecoration(
-              border: Border.all(color: ColorsTheme.borderColor),
-              borderRadius: BorderRadius.circular(20).copyWith(topLeft: Radius.zero),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Text(
-                '${getGreeting()}, what can I do for you?',
-                style: const TextStyle(
-                  color: ColorsTheme.mainFontColor,
-                  fontSize: 25,
-                  fontFamily: 'Cera Pro',
-                ),
-              ),
-            ),
-          ),
-
-          //Suggestions Text
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.only(top: 10, left: 22),
-            child: const Text(
-              'Here are a few suggestions',
-              style: TextStyle(
-                fontFamily: 'Cera Pro',
-                fontSize: 20,
-                color: ColorsTheme.mainFontColor,
-              ),
-            ),
-          ),
-          Column(
-            children: const [
-              FeatureBox(
-                  color: ColorsTheme.firstSuggestionBoxColor,
-                  headerText: 'ChatGPT',
-                  descriptionText: 'Get Information from ChatGPT'),
-              FeatureBox(
-                  color: ColorsTheme.secondSuggestionBoxColor,
-                  headerText: 'Dall-E',
-                  descriptionText: 'Get Images from Dall-E'),
-              FeatureBox(
-                  color: ColorsTheme.thirdSuggestionBoxColor,
-                  headerText: 'TTS',
-                  descriptionText: 'Text2Speech and Speech2Text'),
-            ],
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.mic),
-      ),
-    );
-  }
-}
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 String getGreeting() {
   DateTime time = DateTime.now();
@@ -113,5 +16,204 @@ String getGreeting() {
     return 'Good Afternoon';
   } else {
     return 'Good Evening';
+  }
+}
+
+class HomePage extends StatefulHookConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  final speechToText = SpeechToText();
+  final flutterTts = FlutterTts();
+  String lastWords = '';
+  final OpenAIService openAIService = OpenAIService();
+  String? generatedContent;
+  String? generatedImageUrl;
+  int start = 200;
+  int delay = 200;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initSpeechToText();
+    initTextToSpeech();
+  }
+
+  Future<void> initTextToSpeech() async {
+    await flutterTts.setSharedInstance(true);
+    setState(() {});
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  Future<void> systemSpeak(String content) async {
+    await flutterTts.speak(content);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+    flutterTts.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Assistant'),
+        leading: const Icon(Icons.menu),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Assistant picture
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: ColorsTheme.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 123,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/virtualAssistant.png',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            //Chat bubble
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+              decoration: BoxDecoration(
+                border: Border.all(color: ColorsTheme.borderColor),
+                borderRadius: BorderRadius.circular(20).copyWith(topLeft: Radius.zero),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  generatedContent == null
+                      ? '${getGreeting()}, what can I do for you?'
+                      : generatedContent!,
+                  style: TextStyle(
+                    color: ColorsTheme.mainFontColor,
+                    fontSize: generatedContent == null ? 25 : 18,
+                    fontFamily: 'Cera Pro',
+                  ),
+                ),
+              ),
+            ),
+
+            //Suggestions Text
+            if (generatedImageUrl != null)
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(generatedImageUrl!),
+                ),
+              ),
+            Visibility(
+              visible: generatedContent == null && generatedImageUrl == null,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.only(top: 10, left: 22),
+                child: const Text(
+                  'Here are a few suggestions',
+                  style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    fontSize: 20,
+                    color: ColorsTheme.mainFontColor,
+                  ),
+                ),
+              ),
+            ),
+            Visibility(
+              visible: generatedContent == null && generatedImageUrl == null,
+              child: Column(
+                children: const [
+                  FeatureBox(
+                      color: ColorsTheme.firstSuggestionBoxColor,
+                      headerText: 'ChatGPT',
+                      descriptionText: 'Get Information from ChatGPT'),
+                  FeatureBox(
+                      color: ColorsTheme.secondSuggestionBoxColor,
+                      headerText: 'Dall-E',
+                      descriptionText: 'Get Images from Dall-E'),
+                  FeatureBox(
+                      color: ColorsTheme.thirdSuggestionBoxColor,
+                      headerText: 'TTS',
+                      descriptionText: 'Text2Speech and Speech2Text'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            final speech = await openAIService.isArtPromptAPI(lastWords);
+            if (speech.contains('https')) {
+              generatedImageUrl = speech;
+              generatedContent = null;
+              setState(() {});
+            } else {
+              generatedImageUrl = null;
+              generatedContent = speech;
+              setState(() {});
+              await systemSpeak(speech);
+            }
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
+        backgroundColor: ColorsTheme.firstSuggestionBoxColor,
+        child: const Icon(Icons.mic),
+      ),
+    );
   }
 }
